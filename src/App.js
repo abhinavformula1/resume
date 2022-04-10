7f633f815d31537126d6419fbf3dd594d37e4c9e
abhinav <abhinavformula1@gmail.com>
2022-04-11 01:58:35 +0530

    resume 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header'
 import Certifcations from './components/certifications'
 import Experiences from './components/experiences'
 import Summary from './components/professionalSummary'
+import Skills from './components/technicalSkills'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Header/>
       <Experiences/>
       <Summary/>
+      <Skills/>
       <Certifcations/>
     </div>
   );
---
+++ src/components/technicalSkills.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <h1>Technical Skills</h1>
+      <div>
+        <h3>
+          Force.com: Salesforce Declarative & Customization (Validation Rules,
+          Flow, Process Builder, Platform Event, Custom Setting, Custom
+          Metadata, Trigger, Asynchronous Apex Batch, Queueable, Schedule), Apex
+          Rest Service, Lightning Web Components, Lightning Design System)
+        </h3>
+      </div>
+      <div>
+        <h3>
+          Apttus CPQ : Product, Pricing, Approvals, Constraint Rules, PAR, AVM,
+          Deal Guidance, Multi-Currency, Templates, X-Author Apps (Product,
+          Template, Approval)
+        </h3>
+      </div>
+      <div>
+        <h3>Community : Partner, Community</h3>
+      </div>
+      <div>
+        <h3>Languages : Apex, JavaScript</h3>
+      </div>
+      <div>
+        <h3>
+          Tools : VS Code, SOAP UI, Data Loader, Talend OS for DI, Git, Github,
+          Bitbucket, Jenkins
+        </h3>
+      </div>
+    </div>
+  );
+};
+export default Skills;
