9933c39c30ec0f7fd988e4d6c9952ab2e413ef90
abhinav <abhinavformula1@gmail.com>
2022-04-12 01:58:18 +0530

    navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>
-        Abhinav Kumar
-      </Navbar.Brand>
-    </Navbar> 
-  )
-}
+      <Navbar.Brand>Abhinav Kumar</Navbar.Brand>
+      <Navbar.Brand>abhinavformula1@gmail.com</Navbar.Brand>
+    </Navbar>
+  );
+};
 export default Header;
