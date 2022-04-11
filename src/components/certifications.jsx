03271a113066a0338563fc84f781d460f44b0895
abhinav <abhinavformula1@gmail.com>
2022-04-12 02:11:52 +0530

    certifications

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -3,30 +3,17 @@ import React from "react";
 const Certifcations = () => {
   return (
     <div>
-        <h1>Certification(s)</h1>
+      <h1>Certification(s)</h1>
       <div>
-        <h3>Conga CPQ 201</h3>
-      </div>
-      <div>
-        <h3>Conga CLM 201</h3>
-      </div>
-      <div>
-        <h3>Salesforce Admin</h3>
-      </div>
-      <div>
-        <h3>Salesforce App Builder</h3>
-      </div>
-      <div>
-        <h3>Salesforce Platform Developer 1</h3>
-      </div>
-      <div>
-        <h3>IBM Blockchain Essential</h3>
-      </div>
-      <div>
-        <h3>IBM Blockchain Developer</h3>
+        <p>Conga CPQ 201</p>
+        <p>Conga CLM 201</p>
+        <p>Salesforce Admin</p>
+        <p>Salesforce App Builder</p>
+        <p>Salesforce Platform Developer 1</p>
+        <p>IBM Blockchain Essential</p>
+        <p>IBM Blockchain Developer</p>
       </div>
     </div>
   );
 };
-
 export default Certifcations;
