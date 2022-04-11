6bea256bed5d1abcc2e595c000a2377d54bc8fb5
abhinav <abhinavformula1@gmail.com>
2022-04-12 02:20:16 +0530

    text

--- src/components/experiences.jsx
+++ src/components/experiences.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
 const Experiences = () => {
-    return (
+  return (
+    <div>
+      <h1>Professional Experience</h1>
       <div>
-        <h1>Professional Experience</h1>
-        <div>
-          <h3>
-            Worked with Mindtree limited from July 2013 till April 2016 as Software Engineer.
-          </h3>
-        </div>
-        <div>
-          <h3>
-            Worked with Cognizant from May 2016 to till April 2017 as Software Engineer.
-          </h3>
-        </div>
-        <div>
-          <h3>
-            Working with TCS from April 2017 to till date as Senior Salesforce/Apttus Developer.
-          </h3>
-        </div>
+        <p>
+          Worked with Mindtree limited from July 2013 till April 2016 as Software Engineer.
+        </p>
+        <p>
+          Worked with Cognizant from May 2016 to till April 2017 as Software Engineer.
+        </p>
+        <p>
+          Working with TCS from April 2017 to till date as Salesforce/Apttus Developer.
+        </p>
       </div>
-    );
-  };
-  export default Experiences;
-  
+    </div>
+  );
+};
+export default Experiences;
