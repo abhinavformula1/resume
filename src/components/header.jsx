e0f05f806f2678b5561da2a73ccda8363212d19f
abhinav <abhinavformula1@gmail.com>
2022-04-12 13:21:05 +0530

    navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand>Abhinav Kumar</Navbar.Brand>
-      <Navbar.Brand>abhinavformula1@gmail.com</Navbar.Brand>
+      <Nav>
+        <Nav.Link>Abhinav Kumar</Nav.Link>
+        <Nav.Link>abhinavformula1@gmail.com</Nav.Link>
+      </Nav>
     </Navbar>
   );
 };
