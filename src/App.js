3043e1dc6ffc0fc539641a77887aa62e02a4b3d7
abhinav <abhinavformula1@gmail.com>
2022-04-20 18:49:05 +0530

    Added SVG

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Container } from "react-bootstrap";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import { Container, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Certifcations from "./components/certifications";
@@ -7,19 +14,18 @@ import Summary from "./components/professionalSummary";
 import Skills from "./components/technicalSkills";
 import ProjectsSummary from "./components/projectsSummary";
 import Responsibilities from "./components/responsibilities";
+import star from "./icons/star.svg";
 
 export default function App() {
   return (
-    <div className="App">
-      <Container>
-        <Header />
+    <Container>
+        <Header/>
         <Experiences />
         <Summary />
         <Skills />
         <ProjectsSummary />
         <Responsibilities />
         <Certifcations />
-      </Container>
-    </div>
+    </Container>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Navbar, Nav, Offcanvas, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Nav>
-        <Nav.Link>Abhinav Kumar</Nav.Link>
-        <Nav.Link>abhinavformula1@gmail.com</Nav.Link>
-      </Nav>
-    </Navbar>
+    <>
+      <Navbar bg="dark" variant="dark" expand={false} fixed="top">
+        <Container fluid>
+          <Navbar.Brand href="#">Abhinav</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#aboutme">About me</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 export default Header;
